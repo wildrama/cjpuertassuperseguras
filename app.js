3058ee8d98dc4c9cfd9653ec40ed18334f7e67e6
Wildrama <rambo1bc@hotmail.com>
2023-12-15 06:56:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3004;
+const port = 3066;
 
 // Establecer la carpeta de archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Iniciar el servidor
+// Iniciar el servidor  
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
