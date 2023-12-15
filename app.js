e758cb2082875af090340ba6f8e75b8dac97a28f
Wildrama <rambo1bc@hotmail.com>
2023-12-15 06:38:14 -0300

    Revert "Revert "Initial commit""
    This reverts commit a01d5ef32498aa1a5551441a7ee7a44ca1988771.

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3004;
+
+// Establecer la carpeta de archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para el archivo index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
